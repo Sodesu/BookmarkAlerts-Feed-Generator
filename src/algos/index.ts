bf78d2130bfbc87481301e586e305325c8904a33
Kunmi Ogunfeibo <73042178+Sodesu@users.noreply.github.com>
2024-05-01 10:00:37 +0100

    Update index.ts
    Added requiresAuth Operator which is controlled via the requiresAuth condition in the algorithm file: Currently Set to false

--- src/algos/index.ts
+++ src/algos/index.ts
@@ -1,14 +1,21 @@
-import { AppContext } from '../config'
+// index.ts
+import { AppContext } from '../config';
 import {
-  QueryParams,
-  OutputSchema as AlgoOutput,
-} from '../lexicon/types/app/bsky/feed/getFeedSkeleton'
-import * as whatsAlf from './whats-alf'
+    QueryParams,
+    OutputSchema as AlgoOutput,
+} from '../lexicon/types/app/bsky/feed/getFeedSkeleton';
+import * as whatsAlf from './whats-alf';
 
-type AlgoHandler = (ctx: AppContext, params: QueryParams) => Promise<AlgoOutput>
+type AlgoHandler = {
+    handler: (ctx: AppContext, params: QueryParams) => Promise<AlgoOutput>,
+    requiresAuth: boolean
+};
 
 const algos: Record<string, AlgoHandler> = {
-  [whatsAlf.shortname]: whatsAlf.handler,
-}
+    [whatsAlf.shortname]: {
+        handler: whatsAlf.handler,
+        requiresAuth: whatsAlf.requiresAuth,  // Reflects the updated auth requirement
+    },
+};
 
-export default algos
+export default algos;
