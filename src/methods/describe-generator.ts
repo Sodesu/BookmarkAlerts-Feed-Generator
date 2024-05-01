97dce96c48de17a7d579dcbd1eeb964ac71c6fd5
Kunmi Ogunfeibo <73042178+Sodesu@users.noreply.github.com>
2024-05-01 09:43:39 +0100

    Update describe-generator.ts
    - Included error handling method

--- src/methods/describe-generator.ts
+++ src/methods/describe-generator.ts
@@ -5,19 +5,25 @@ import { AtUri } from '@atproto/syntax'
 
 export default function (server: Server, ctx: AppContext) {
   server.app.bsky.feed.describeFeedGenerator(async () => {
-    const feeds = Object.keys(algos).map((shortname) => ({
-      uri: AtUri.make(
-        ctx.cfg.publisherDid,
-        'app.bsky.feed.generator',
-        shortname,
-      ).toString(),
-    }))
-    return {
-      encoding: 'application/json',
-      body: {
-        did: ctx.cfg.serviceDid,
-        feeds,
-      },
+    try {
+      const feeds = Object.keys(algos).map((shortname) => {
+        const uri = AtUri.make(
+          ctx.cfg.publisherDid,
+          'app.bsky.feed.generator',
+          shortname,
+        ).toString();
+        return { uri };
+      });
+      return {
+        encoding: 'application/json',
+        body: {
+          did: ctx.cfg.serviceDid,
+          feeds,
+        },
+      };
+    } catch (error) {
+      console.error('Failed to describe feed generator:', error);
+      throw error;
     }
   })
 }
