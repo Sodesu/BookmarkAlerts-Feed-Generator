0f1de3dbf93b3a00554cf93adf14e103b4e211dc
Kunmi Ogunfeibo <73042178+Sodesu@users.noreply.github.com>
2024-05-02 23:37:08 +0100

    Update index.ts
    Adjusted for test execution

--- src/index.ts
+++ src/index.ts
@@ -2,34 +2,24 @@ import dotenv from 'dotenv'
 import FeedGenerator from './server'
 
 const run = async () => {
-  dotenv.config()
-  const hostname = maybeStr(process.env.FEEDGEN_HOSTNAME) ?? 'example.com'
-  const serviceDid =
-    maybeStr(process.env.FEEDGEN_SERVICE_DID) ?? `did:web:${hostname}`
+  dotenv.config();
+  const hostname = maybeStr(process.env.FEEDGEN_HOSTNAME, 'example.com');
+  const serviceDid = maybeStr(process.env.FEEDGEN_SERVICE_DID, `did:web:${hostname}`);
   const server = FeedGenerator.create({
-    port: maybeInt(process.env.FEEDGEN_PORT) ?? 3000,
-    listenhost: maybeStr(process.env.FEEDGEN_LISTENHOST) ?? 'localhost',
-    databaseUrl: maybeStr(process.env.FEEDGEN_DATABASE_URL), // PostgreSQL connection string
-    subscriptionEndpoint:
-      maybeStr(process.env.FEEDGEN_SUBSCRIPTION_ENDPOINT) ??
-      'wss://bsky.network',
-    publisherDid:
-      maybeStr(process.env.FEEDGEN_PUBLISHER_DID) ?? 'did:example:alice',
-    subscriptionReconnectDelay:
-      maybeInt(process.env.FEEDGEN_SUBSCRIPTION_RECONNECT_DELAY) ?? 3000,
+    port: maybeInt(process.env.FEEDGEN_PORT, 3000),
+    listenhost: maybeStr(process.env.FEEDGEN_LISTENHOST, 'localhost'),
+    databaseUrl: maybeStr(process.env.FEEDGEN_DATABASE_URL),
+    subscriptionEndpoint: maybeStr(process.env.FEEDGEN_SUBSCRIPTION_ENDPOINT, 'wss://bsky.network'),
+    publisherDid: maybeStr(process.env.FEEDGEN_PUBLISHER_DID, 'did:example:alice'),
+    subscriptionReconnectDelay: maybeInt(process.env.FEEDGEN_SUBSCRIPTION_RECONNECT_DELAY, 3000),
     hostname,
     serviceDid,
   })
-  await server.start()
-  console.log(
-    `🤖 running feed generator at http://${server.cfg.listenhost}:${server.cfg.port}`,
-  )
+  await server.start();
+  console.log(`🤖 running feed generator at http://${server.cfg.listenhost}:${server.cfg.port}`);
 }
 
-const maybeStr = (val?: string) => {
-  if (!val) return undefined
-  return val
-}
+const maybeStr = (val?: string, defaultValue: string = '') => val || defaultValue;
 
 const maybeInt = (val?: string) => {
   if (!val) return undefined
