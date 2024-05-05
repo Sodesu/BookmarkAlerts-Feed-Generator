8d8bf304e441089e3f55eec555d204e4cde6e294
Kunmi Ogunfeibo <73042178+Sodesu@users.noreply.github.com>
2024-05-05 17:07:19 +0100

    Update index.ts
    Modified script to allow SSL connection upon execution of the compiler

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,16 +2,25 @@ import { Kysely, Migrator, PostgresDialect } from 'kysely';
 import { DatabaseSchema } from './schema';
 import { migrationProvider } from './migrations';
 import { Pool } from 'pg';
+import fs from 'fs';
 
-export const createDb = (): Database => {
+
+
+export const createDb = (): Kysely<DatabaseSchema> => {
+  const pool = new Pool({
+    connectionString: process.env.FEEDGEN_DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: true,
+      ca: fs.readFileSync('path/to/aws-rds-ca-cert.pem') // Adjust the path to your AWS RDS CA certificate
+    }
+  });
+
   return new Kysely<DatabaseSchema>({
     dialect: new PostgresDialect({
-      pool: new Pool({
-        connectionString: process.env.FEEDGEN_DATABASE_URL
-      }),
+      pool: pool
     }),
-  })
-}
+  });
+};
 
 export const migrateToLatest = async (db: Database) => {
   const migrator = new Migrator({ db, provider: migrationProvider })
