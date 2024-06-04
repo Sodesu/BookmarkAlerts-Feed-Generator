60c9853b7e1423e530a29454119a188bd53632b8
Kunmi Ogunfeibo <73042178+Sodesu@users.noreply.github.com>
2024-06-04 22:25:25 +0100

    Update subscription.ts
    Modified to accommodate additional data from Feed Generator

--- src/subscription.ts
+++ src/subscription.ts
@@ -1,8 +1,8 @@
 import {
   OutputSchema as RepoEvent,
   isCommit,
-} from './lexicon/types/com/atproto/sync/subscribeRepos'
-import { FirehoseSubscriptionBase, getOpsByType } from './util/subscription'
+} from './lexicon/types/com/atproto/sync/subscribeRepos';
+import { FirehoseSubscriptionBase, getOpsByType } from './util/subscription';
 
 export class FirehoseSubscription extends FirehoseSubscriptionBase {
   async handleEvent(evt: RepoEvent) {
@@ -11,13 +11,20 @@ export class FirehoseSubscription extends FirehoseSubscriptionBase {
 
     const postsToDelete = ops.posts.deletes.map((del) => del.uri);
     const postsToCreate = ops.posts.creates
-      .filter((create) => create.record.text.toLowerCase().includes('alf'))
+      .filter((create) => create.record.text.toLowerCase().includes('@bookmarkalerts'))
       .map((create) => ({
           uri: create.uri,
           cid: create.cid,
+          content: create.record.text, // Store the post content
           replyParent: create.record?.reply?.parent.uri ?? null,
           replyRoot: create.record?.reply?.root.uri ?? null,
           indexedAt: new Date().toISOString(),
+          parent_uri: create.record?.reply?.parent.uri ?? null, // Store parent post details
+          parent_cid: create.record?.reply?.parent.cid ?? null,
+          parent_content: create.record?.reply?.parent.text ?? null,
+          parent_replyParent: create.record?.reply?.parent.replyParent ?? null,
+          parent_replyRoot: create.record?.reply?.parent.replyRoot ?? null,
+          parent_indexedAt: create.record?.reply?.parent.indexedAt ?? null,
       }));
 
     try {
