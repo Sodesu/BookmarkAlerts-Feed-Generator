feda73820bf5e9e79492d670e0bdd202f0f7d4d8
Kunmi Ogunfeibo <73042178+Sodesu@users.noreply.github.com>
2024-06-04 22:28:58 +0100

    Update subscription.ts
    Modified to accommodate additional data parameters from Feed Generator

--- src/util/subscription.ts
+++ src/util/subscription.ts
@@ -1,20 +1,17 @@
-import { Subscription } from '@atproto/xrpc-server'
-import { cborToLexRecord, readCar } from '@atproto/repo'
-import { BlobRef } from '@atproto/lexicon'
-import { ids, lexicons } from '../lexicon/lexicons'
-import { Record as PostRecord } from '../lexicon/types/app/bsky/feed/post'
+import { Subscription } from '@atproto/xrpc-server';
+import { cborToLexRecord, readCar } from '@atproto/repo';
+import { BlobRef } from '@atproto/lexicon';
+import { ids, lexicons } from '../lexicon/lexicons';
+import { Record as PostRecord } from '../lexicon/types/app/bsky/feed/post';
 import {
   Commit,
   OutputSchema as RepoEvent,
   isCommit,
-} from '../lexicon/types/com/atproto/sync/subscribeRepos'
-import { Database } from '../db'
-
-
-
+} from '../lexicon/types/com/atproto/sync/subscribeRepos';
+import { Database } from '../db';
 
 export abstract class FirehoseSubscriptionBase {
-  public sub: Subscription<RepoEvent>
+  public sub: Subscription<RepoEvent>;
 
   constructor(public db: Database, public service: string) {
     this.sub = new Subscription({
@@ -26,17 +23,15 @@ export abstract class FirehoseSubscriptionBase {
           return lexicons.assertValidXrpcMessage<RepoEvent>(
             ids.ComAtprotoSyncSubscribeRepos,
             value,
-          )
+          );
         } catch (err) {
-          console.error('repo subscription skipped invalid message', err)
+          console.error('repo subscription skipped invalid message', err);
         }
       },
-    })
+    });
   }
 
-  abstract handleEvent(evt: RepoEvent): Promise<void>
-
-
+  abstract handleEvent(evt: RepoEvent): Promise<void>;
 
   async run(subscriptionReconnectDelay: number, maxDelay: number = 32000) {
     let delay = subscriptionReconnectDelay;
@@ -59,13 +54,12 @@ export abstract class FirehoseSubscriptionBase {
     }
   }
 
-
   async updateCursor(cursor: number) {
     await this.db
       .updateTable('sub_state')
       .set({ cursor })
       .where('service', '=', this.service)
-      .execute()
+      .execute();
   }
 
   async getCursor(): Promise<{ cursor?: number }> {
@@ -73,93 +67,93 @@ export abstract class FirehoseSubscriptionBase {
       .selectFrom('sub_state')
       .selectAll()
       .where('service', '=', this.service)
-      .executeTakeFirst()
-    return res ? { cursor: res.cursor } : {}
+      .executeTakeFirst();
+    return res ? { cursor: res.cursor } : {};
   }
 }
 
-
 export const getOpsByType = async (evt: Commit): Promise<OperationsByType> => {
-  const car = await readCar(evt.blocks)
+  const car = await readCar(evt.blocks);
   const opsByType: OperationsByType = {
     posts: { creates: [], deletes: [] },
-  }
+  };
 
   for (const op of evt.ops) {
-    const uri = `at://${evt.repo}/${op.path}`
-    const [collection] = op.path.split('/')
+    const uri = `at://${evt.repo}/${op.path}`;
+    const [collection] = op.path.split('/');
 
-    if (op.action === 'update') continue // updates not supported yet
+    if (op.action === 'update') continue; // updates not supported yet
 
     if (op.action === 'create') {
-      if (!op.cid) continue
-      const recordBytes = car.blocks.get(op.cid)
-      if (!recordBytes) continue
-      const record = cborToLexRecord(recordBytes)
-      const create = { uri, cid: op.cid.toString(), author: evt.repo }
+      if (!op.cid) continue;
+      const recordBytes = car.blocks.get(op.cid);
+      if (!recordBytes) continue;
+      const record = cborToLexRecord(recordBytes);
+      const create = { uri, cid: op.cid.toString(), author: evt.repo };
       if (collection === ids.AppBskyFeedPost && isPost(record)) {
-        opsByType.posts.creates.push({ record, ...create })
+        opsByType.posts.creates.push({ record, ...create });
       }
     }
 
     if (op.action === 'delete') {
       if (collection === ids.AppBskyFeedPost) {
-        opsByType.posts.deletes.push({ uri })
+        opsByType.posts.deletes.push({ uri });
       }
     }
   }
 
-  return opsByType
-}
+  return opsByType;
+};
 
 type OperationsByType = {
-  posts: Operations<PostRecord>
-}
+  posts: Operations<PostRecord>;
+};
 
 type Operations<T = Record<string, unknown>> = {
-  creates: CreateOp<T>[]
-  deletes: DeleteOp[]
-}
+  creates: CreateOp<T>[];
+  deletes: DeleteOp[];
+};
 
 type CreateOp<T> = {
-  uri: string
-  cid: string
-  author: string
-  record: T
-}
+  uri: string;
+  cid: string;
+  author: string;
+  record: T;
+};
 
 type DeleteOp = {
-  uri: string
-}
+  uri: string;
+};
 
 export const isPost = (obj: unknown): obj is PostRecord => {
-  return isType(obj, ids.AppBskyFeedPost)
-}
+  return isType(obj, ids.AppBskyFeedPost);
+};
 
 const isType = (obj: unknown, nsid: string) => {
   try {
-    lexicons.assertValidRecord(nsid, fixBlobRefs(obj))
-    return true
+    lexicons.assertValidRecord(nsid, fixBlobRefs(obj));
+    return true;
   } catch (err) {
-    return false
+    return false;
   }
-}
+};
 
 const fixBlobRefs = (obj: unknown): unknown => {
   if (Array.isArray(obj)) {
-    return obj.map(fixBlobRefs)
+    return obj.map(fixBlobRefs);
   }
   if (obj && typeof obj === 'object') {
     if (obj.constructor.name === 'BlobRef') {
-      const blob = obj as BlobRef
-      return new BlobRef(blob.ref, blob.mimeType, blob.size, blob.original)
+      const blob = obj as BlobRef;
+      return new BlobRef(blob.ref, blob.mimeType, blob.size, blob.original);
     }
     return Object.entries(obj).reduce((acc, [key, val]) => {
-      return Object.assign(acc, { [key]: fixBlobRefs(val) })
-    }, {} as Record<string, unknown>)
+      return Object.assign(acc, { [key]: fixBlobRefs(val) });
+    }, {} as Record<string, unknown>);
   }
-  return obj
-}
+  return obj;
+};
+
 
 
 
